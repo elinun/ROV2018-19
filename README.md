a5a1cf47c61e180adce70a35d608a8a7108c25a2
Eli Nunez <enunez@iscribehealth.com>
2018-10-21 17:03:05 -0400

    Got the Controller models started.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ROV2018-19
-THE ROV repository for the 2018-19 school year.
+THE ROV repository for the 2018-19 school year. <a href="https://sites.google.com/trojanstudent.net/innovocean-docs">Documentation</a>
--- ROV2019/Models/Controller.cs
+++ ROV2019/Models/Controller.cs
@@ -6,12 +6,39 @@ using System.Threading.Tasks;
 
 namespace ROV2019.Models
 {
-    public class Controllers
-    {
-        public List<Controller> SavedControllers { get; set; }
-    }
     public abstract class Controller
     {
+        public int[] Thrusters { get; }
+        public bool[] Buttons { get; }
+        public int[] Switches { get; }
+        public int[] IntegratedTrim { get; }
+        public object[] Miscelaneous { get; }
 
+        public ValueChanged OnValueChanged { get; set; }
+        public IntChanged OnThrusterChanged { get; set; }
+        public ButtonChanged OnButtonChanged { get; set; }
+        public IntChanged OnSwitchChanged { get; set; }
+        public IntChanged OnTrimChanged { get; set; }
+        public MiscelaneousChanged OnMiscelaneousChanged { get; set; }
+
+        public interface MiscelaneousChanged
+        {
+            void OnMiscelaneousChanged(int index, object oldValue, object newValue);
+        }
+
+        public interface ValueChanged
+        {
+            void OnValueChanged(Controller c);
+        }
+
+        public interface IntChanged
+        {
+            void OnIntValueChanged(int index, int oldValue, int newValue);
+        }
+
+        public interface ButtonChanged
+        {
+            void OnButtonChanged(int index, bool oldValue, bool newValue);
+        }
     }
 }
